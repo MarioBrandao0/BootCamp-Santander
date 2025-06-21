34f6c9c004c4deb45b8d440a3855a16b172d8507
MarioBrandao0 <mariorochedo329@gmail.com>
2025-06-21 13:40:46 -0300

    Aprendendo Operadores e tipos primitivos

---
+++ AprendendoJava/src/javas/programacao/de/linguagem/fundamentos/OperadoresJava.java
@@ -0,0 +1,53 @@
+package javas.programacao.de.linguagem.fundamentos;
+
+import java.util.Scanner;
+
+public class OperadoresJava {
+    static Scanner x = new Scanner(System.in);// o = é o operador de atribuição
+    public static void main(String[] args) {
+
+       MaiorEMenor();
+
+        /* System.out.println("Quanto que é 2 + 2?");
+        int age = x.nextInt();
+
+        boolean isWrong = age != 4;
+
+        System.out.printf("O resultado é 4, vc acertou??\nSua Resposta -> (%s)", age == 4 ? "Acertou" : "Errou");//Podemos fazer comparações direto na impressão
+
+        System.out.printf("O resultado é 4, vc acertou??\nSua Resposta -> (%s)", !isWrong);//Aqui estamos negando a reposta da variavel isWrong
+        */
+    }
+
+
+    public static void MaiorEMenor() {
+        System.out.println("Você tem quantos anos? ");
+        int age = x.nextInt();
+
+        System.out.println("Você é emacipado?");
+        boolean isEmamcipated = x.nextBoolean();
+
+
+        boolean canDrive = age >= 18 || (isEmamcipated && age >= 16);//Podemos inverter o valor ja na propria expressão, sem precisar declarar novamente
+
+        System.out.println(canDrive? "Voce pode dirigir" : "Você não pode dirigir");
+
+    }
+
+    public static void tabelaVerdade() {
+        System.out.println("Operador && (E lógico)");
+        System.out.printf("true  && true  = %s\n", true && true);
+        System.out.printf("true  && false = %s\n", true && false);
+        System.out.printf("false && true  = %s\n", false && true);
+        System.out.printf("false && false = %s\n", false && false);
+
+        System.out.println("-".repeat(90));
+
+        System.out.println("\nOperador || (OU lógico)");
+        System.out.printf("true  || true  = %s\n", true || true);
+        System.out.printf("true  || false = %s\n", true || false);
+        System.out.printf("false || true  = %s\n", false || true);
+        System.out.printf("false || false = %s\n", false || false);
+    }
+
+}
---
+++ AprendendoJava/src/javas/programacao/de/linguagem/fundamentos/Primitivos.java
@@ -0,0 +1,13 @@
+package javas.programacao.de.linguagem.fundamentos;
+
+public class Primitivos {
+    public static void main(String[] args) {
+        byte numberByte = 127; //De -127 a 127
+        int numberInter = 1; //De -2.147.483.648 a 2.147.483.648
+        long numberLong = 1L; // De -9.223.372.036.854.775.808 a 9.223.372.036.854.775.807.
+        float numberFloat= 1.0F; // sem o F no final ele entende que é um double
+        char caracter = 'a'; //Ele so aceita apenas um unico caracter. Os caracteres são colocados entre aspas simples
+        char numChar = 164;//Char tambem pode ser numero
+        boolean valorBoleano = true;//Valores boleanos são apenas verdadeiros ou falsos
+    }
+}
