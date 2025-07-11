72076a0584c07c31335665f4272bf25f9ddc5e9a
MarioBrandao0 <mariorochedo329@gmail.com>
2025-07-10 23:21:54 -0300

    Inicio da aula sobre tratamento de exceptions

--- AprendendoJava/src/fundamentos/TratamentoExecptions.java
+++ AprendendoJava/src/fundamentos/TratamentoExecptions.java
@@ -5,20 +5,6 @@ import java.util.Scanner;
 public class TratamentoExecptions {
     private static Scanner x = new Scanner(System.in);
     public static void main(String[] args) {
-        try {
-            test();
-        } catch (Exception e) {
-            e.printStackTrace();
-            System.out.println("Erro ao executar");
-        }
-    }
 
-    private static void test() {
-        System.out.println("Digite seu nome: ");
-        String nome = x.next();
-
-        if (!nome.equalsIgnoreCase("Mario")) throw new RuntimeException();//Podemos forçar uma Exception passando o tipó dela e tratando na classe onde esta sendo chamada
-
-        System.out.println("Olá Mario");
     }
 }
---
+++ AprendendoJava/src/tratamentoDeExceptions/dao/UserDAO.java
@@ -0,0 +1,50 @@
+package tratamentoDeExceptions.dao;
+
+import tratamentoDeExceptions.exceptions.UserNotFoundException;
+import tratamentoDeExceptions.mode.UserModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+
+    private long nextId = 1L;
+
+    private final List<UserModel> listModels = new ArrayList<>();
+
+
+    /**
+     * Recebe um model de user e salva ele na listModels e retorna o model salvo
+     * @param model
+     * @return Usermodel
+     */
+    public UserModel save(final UserModel model) {
+        model.setId(nextId++);
+        listModels.add(model);
+        return model;
+    }
+
+    public UserModel update(final UserModel model) {
+        UserModel updatedModel = findById(model.getId());
+        listModels.remove(updatedModel);
+        listModels.add(model);
+        return model;
+    }
+
+    public void delete(final long id) {
+        UserModel deletedModel = findById(id);
+        listModels.remove(deletedModel);
+    }
+
+    public UserModel findById(final long id) {
+        String message = "Usuário não encontrado";
+        return listModels.stream()
+                .filter(user -> user.getId() == id)
+                .findFirst().orElseThrow(() -> new UserNotFoundException(message));
+    }
+
+    public List<UserModel> findAll() {
+        return listModels.isEmpty() ? null : listModels;
+    }
+
+}
